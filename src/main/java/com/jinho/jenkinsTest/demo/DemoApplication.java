998d23aaaca2b23716472067c2f1afe2c89c8c9d
Cho Jin Ho <whwlsgh1111@naver.com>
2023-10-04 15:48:47 +0900

    Update DemoApplication.java
    gradle test

--- src/main/java/com/jinho/jenkinsTest/demo/DemoApplication.java
+++ src/main/java/com/jinho/jenkinsTest/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("ad");
+		System.out.println("빠밤");
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
